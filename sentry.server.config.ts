116bc6ce3fb8775538352760fcd313d3ecdc7d26
mukesh mahla <mukesh.23it647@rtu.ac.in>
2026-02-17 01:07:06 +0530

    added error tracking

---
+++ sentry.server.config.ts
@@ -0,0 +1,25 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://ce01fe7a57c26764819af753447a8e00@o4510896963518464.ingest.de.sentry.io/4510897186603088",
+
+  integrations:[
+Sentry.vercelAIIntegration({
+  recordInputs:true,
+  recordOutputs:true,
+})
+  ],
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -1,31 +1,37 @@
 import prisma from "@/lib/db";
 import { inngest } from "./client";
 import Groq from "groq-sdk";
-
+import * as Sentry from "@sentry/nextjs"
 const groq = new Groq({
   apiKey: process.env.GROQ_API_KEY,
+});
+ const client = Sentry.instrumentGoogleGenAIClient(groq, {
+  recordInputs: true,
+  recordOutputs: true,
 });
 
 export const executeAi = inngest.createFunction(
   { id: "execute-ai" },
   { event: "execute/ai" },
+
+ 
   async ({ event,step }) => {
          await step.sleep("pretend","5s")
     const airesponse = await step.run("groq-generate-text", async () => {
-      return await groq.chat.completions.create({
+      return await client.chat.completions.create({
         model: "llama-3.3-70b-versatile",
         messages: [
           {
             role: "user",
             content: "What is n8n?",
           },
+            
         ],
+      
       });
     });
 
     const text = airesponse.choices[0].message.content;
-
-    console.log(text);
 
     return text;
   }
