ac97f85261de71aab7269c7e2a3fc578deab2324
mukesh mahla <mukesh.23it647@rtu.ac.in>
2026-03-06 15:32:26 +0530

    node templeting, dynamic variable

--- src/features/executions/components/http-request/executer.ts
+++ src/features/executions/components/http-request/executer.ts
@@ -1,11 +1,18 @@
 import type { NodeExecuter } from "@/features/executions/types";
 import { NonRetriableError } from "inngest";
 import ky,{type Options as kyOptions} from "ky"
+import Handlebars from "handlebars"
+
+ Handlebars.registerHelper("json",(context)=>{
+   const jsonstringfy = JSON.stringify(context,null,2)
+   const safeString = new Handlebars.SafeString(jsonstringfy)
+   return safeString
+})
 
 type httpRequestData = {
-   variableName?:string
-   endpoint?:string,
-   method?:"GET" | "POST" | "PUT" | "DELETE" | "PATCH",
+   variableName:string
+   endpoint:string,
+   method:"GET" | "POST" | "PUT" | "DELETE" | "PATCH",
    body?:string
 }
 export const httpRequestExecuter:NodeExecuter<httpRequestData> = async ({data,context,step,nodeId})=>{
@@ -17,18 +24,24 @@ export const httpRequestExecuter:NodeExecuter<httpRequestData> = async ({data,co
    if(!data.variableName){
       throw new NonRetriableError("variable name not configured ")
    }
+   
+   if(!data.variableName){
+      throw new NonRetriableError("method not configured not configured ")
+   }
 
   const result =  await step.run("http-request",async ()=>{
-   const endpoint = data.endpoint! 
-   const method = data.method || "GET"
+   const endpoint = Handlebars.compile(data.endpoint)(context)
+   const method = data.method 
 
    const options:kyOptions = {method}
 
    if(["POST","PUT","PATCH"].includes(method) && data.body){
-      if(data.body){
-         options.body = data.body
+      const resolved = Handlebars.compile(data.body || "{}")(context)
+      JSON.parse(resolved)
+     
+         options.body = resolved
          options.headers = {"Content-Type":"application/json"
-         }
+         
       }
    }
 
@@ -46,7 +59,7 @@ export const httpRequestExecuter:NodeExecuter<httpRequestData> = async ({data,co
 
    return {
       ...context,
-      [data.variableName!]:responsePayload
+      [data.variableName]:responsePayload
 }
   })
     
--- src/features/executions/lib/executer-registry.ts
+++ src/features/executions/lib/executer-registry.ts
@@ -3,10 +3,11 @@ import { NodeExecuter } from "../types";
 import { manualTriggerExecuter } from "@/features/triggers/components/manual-trigger/executer";
 import { httpRequestExecuter } from "../components/http-request/executer";
 
-export const execuerRegistry: Record<NodeType,NodeExecuter> = {
+
+export const execuerRegistry: Record<NodeType,NodeExecuter<any>> = {
 [NodeType.MANUAL_TRIGGER]:manualTriggerExecuter,
 [NodeType.INITIAL]:manualTriggerExecuter,
-[NodeType.HTTP_REQUEST]:httpRequestExecuter // for now we can use the same executer for http request, but in future we will create a separate executer for it
+[NodeType.HTTP_REQUEST]:httpRequestExecuter 
 
 }
 
